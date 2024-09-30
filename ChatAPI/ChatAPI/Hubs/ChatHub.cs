21d7b6b2455c6f003e80c617bee282569bfe8bb8
OFFICE\pwalunj <pwalunj@csod.com>
2024-09-30 09:51:59 +0530

    Local set up for API and react ui

---
+++ ChatAPI/ChatAPI/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using ChatAPI.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatAPI.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinChat(UserConnection connection)
+        {
+            await Clients.All.SendAsync(method: "ReceiveMessage", arg1: "admin", arg2: $"{connection.UserName} has joined");
+        }
+
+        public async Task JoinSpecificChatRoom(UserConnection connection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName: connection.ChatRoom);
+            await Clients.Groups(connection.ChatRoom).SendAsync(method: "ReceiveMessage", arg1: "admin", arg2: $"{connection.UserName} has joined {connection.ChatRoom}");
+        }
+
+    }
+}
